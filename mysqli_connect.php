d46908278e26d4d3887145dabd714fc6b136cb03
Brad <94862922+BBro5th@users.noreply.github.com>
2021-12-07 16:37:59 -0500

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -4,10 +4,10 @@
 // This file establishes a connection to MySQL and selects the database.
 
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'bwb6403');
-DEFINE ('DB_PASSWORD', 'bDWIYKEzl');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'bwb6403');
+DEFINE ('DB_USER', 'Username'); //Username placeholder
+DEFINE ('DB_PASSWORD', 'password'); //password placeholder
+DEFINE ('DB_HOST', 'localhost'); //will require being on same network as DB
+DEFINE ('DB_NAME', 'DatabaseName');
 
 // Make the connection:
 $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
